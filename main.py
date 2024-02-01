62971ea05320521fff80589ae4939772a4add257
barton-jamespaul <barton-jamespaul@proton.me>
2024-02-01 14:58:03 +0900

    Filter and output data

--- main.py
+++ main.py
@@ -2,103 +2,82 @@ import requests
 import json
 from urllib import parse
 import base64
+import configparser
+import pandas as pd
 
 
-def get_auth_token():
+def get_auth_token(auth_config):
+    key_config = configparser.ConfigParser()
+    key_config.read('private_auth.ini')
     app_settings = {
-        'client_id': 'JamesBar-Finding-PRD-ee5603d2f-e4572c3c',
-        'client_secret': 'PRD-e5603d2f399a-ca5c-4d10-bc74-bd9c'}
+        'client_id': key_config['DEFAULT']['client_id'],
+        'client_secret': key_config['DEFAULT']['client_secret']}
     authorization = base64.b64encode(bytes(app_settings['client_id'] + ":" + app_settings['client_secret'], "ISO-8859-1")).decode("ascii")
 
     auth_headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": auth_config['content_type'],
         "Authorization": "Basic " + authorization
     }
-
     body = {
-        "grant_type": "client_credentials",
-        "scope": "https://api.ebay.com/oauth/api_scope"
+        "grant_type": auth_config['grant_type'],
+        "scope": auth_config['scope']
     }
-
     data = parse.urlencode(body)
-
-    token_url = "https://api.ebay.com/identity/v1/oauth2/token"
+    token_url = auth_config['token_url']
 
     auth_response = requests.post(token_url, headers=auth_headers, data=data)
-    return auth_response.json()
+    return auth_response.json()['access_token']
 
 
-def construct_header(token):
-    return {'X-EBAY-C-MARKETPLACE-ID': 'EBAY_GB',
-            'X-EBAY-C-ENDUSERCTX': 'affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>',
-            'Authorization': 'Bearer ' + token
+def construct_header(auth_token, header_config):
+    return {'X-EBAY-C-MARKETPLACE-ID': header_config['market_place'],
+            'X-EBAY-C-ENDUSERCTX': header_config['affiliate'],
+            'Authorization': 'Bearer ' + auth_token
             }
 
 
-def construct_url():
-    base_url = 'https://api.ebay.com/buy/browse/v1/item_summary/search'
-    query = '?q=google pixel 7 128GB&category_ids=9355'
-    max_items = 50
-    limit = f'&limit={max_items}'
-    sort = f'&sort=price'
-    url = f'{base_url}{query}{limit}{sort}'
-    filters = (
-        #'bidCount:[10]',
-        'buyingOptions:{FIXED_PRICE|BEST_OFFER}',
-        #'buyingOptions:{FIXED_PRICE|BEST_OFFER|AUCTION}',
-        'conditionIds:{3000|4000|5000|6000}',
-        'deliveryCountry:GB',
-        'deliveryPostalCode:CT202RQ',
-        'itemLocationCountry:GB',
-        #'priceCurrency:GBP',
-        'searchInDescription:true',
-        'sellerAccountTypes:{INDIVIDUAL}',
-        'lastSoldDate:[2024-01-20T00:00:00Z..2024-01-30T00:00:00Z]'
-    )
-    filter_str = '&filter='
-    for filter in filters:
-        filter_str+=f'{filter},'
-
-    return url+filter_str
+def make_request(url_string, header_string):
+    print(f'Query string: {url_string}')
+    r = requests.get(url_string, headers=header_string)
+    print(f'Query response: {r.status_code}')
+    r_json = r.json()
+    #print(json.r_json(items, indent=4))
+    return r_json
 
 
 def get_all_data(url_value, header_value):
-    r = requests.get(url_value, headers=header_value)
-    parsed = r.json()
-    total_items = parsed['total']
-    total_returned = len(parsed['itemSummaries'])
-    print(total_items, total_returned)
-    print(json.dumps(parsed, indent=4))
-    new_parsed = parsed
+    items = make_request(url_value, header_value)
+    all_items = items
 
-    while total_returned < total_items:
-        next_url = new_parsed['next']
-        print(next_url)
-        r = requests.get(next_url, headers=headers)
-        print(f'{r.status_code}')
-        new_parsed = r.json()
-        print(json.dumps(new_parsed, indent=4))
-        parsed['itemSummaries'].extend(new_parsed['itemSummaries'])
-        total_returned += len(new_parsed['itemSummaries'])
-        print(total_items, total_returned, next_url)
-    #print(json.dumps(parsed, indent=4))
-    return parsed
+    while "next" in items:
+        next_query = items['next']
+        items = make_request(next_query, header_value)
+        all_items['itemSummaries'].extend(items['itemSummaries'])
+    print(f'Count of items returned: {len(all_items['itemSummaries'])}')
+    #print(json.dumps(all_items, indent=4))
+    return all_items['itemSummaries']
+
+def write_data(json_to_write):
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(json_to_write, f, ensure_ascii=False, indent=4)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    response = get_auth_token()
-    #print(response)
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-    headers = construct_header(response['access_token'])
-    url = construct_url()
-    #print(url)
-    json_data = get_all_data(url, headers)
+    token = get_auth_token(config['auth'])
+    headers = construct_header(token, config['header'])
 
-    #r = requests.get(url,headers=headers)
-    #print(f'{r.status_code}')
-    #parsed = r.json()
-    #print(f'{parsed['total']}')
-    #print(json.dumps(parsed, indent=4))
+    items_as_json = get_all_data(config['params']['query'], headers)
 
+    write_data(items_as_json)
 
+    df = pd.json_normalize(items_as_json)
+    stripped_df = df.drop(['leafCategoryIds', 'categories', 'seller.username', 'conditionId', 'thumbnailImages',
+                           'epid','itemAffiliateWebUrl', 'itemHref', 'itemLocation.country', 'additionalImages',
+                           'adultOnly', 'legacyItemId', 'availableCoupons', 'topRatedBuyingExperience',
+                           'priorityListing', 'listingMarketplaceId', 'itemId', 'image.imageUrl',
+                           'seller.sellerAccountType'], axis=1)
+    stripped_df.info()
